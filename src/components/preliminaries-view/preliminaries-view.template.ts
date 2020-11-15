b16729913270aba4321bf74fbe55323dad3f3785
raoulesd <raoulesd@gmail.com>
2020-11-15 16:43:53 +0100

    Update preliminaries-view.template.ts
    added links of documents from preliminaries

--- src/components/preliminaries-view/preliminaries-view.template.ts
+++ src/components/preliminaries-view/preliminaries-view.template.ts
@@ -4,16 +4,15 @@ import { BapcView } from './preliminaries-view.component';
 export default function template(this: BapcView) {
   return html`
     <section>
-      <h1>Preliminaries</h1>
+      <h1>Preliminaries 2020</h1>
       <p>There are a limited amount of slots per institution available for teams and there are usually more teams then slots.
       Therefor preliminaries can be held in your institution to determine which teams qualify for the BAPC 2020. The preliminaries
-      will be held on <strong>Saturday November 14th</strong>.</p>
+      were held on <strong>Saturday November 14th</strong>.</p>
 
-      <p>The following institutions held preliminaries in 2019. Once we have confirmed the preliminaries of 2020, we will update the
-      page with a link to their local website. If you want to organize a preliminary contest or want to train with the preliminary set
-      if you don&apos;t hold a prelimary contest, please <a href="/contact">contact us</a>.</p>    </section>
+	  <p> Here, you can find links to <a href="https://commissies.ch.tudelft.nl/chipcie/archief/2020/dapc/problemset.pdf">the problem set</a>, <a href="https://commissies.ch.tudelft.nl/chipcie/archief/2020/dapc/solutions.pdf">the solutions presentation</a> and the <a href="https://commissies.ch.tudelft.nl/chipcie/archief/2020/dapc/solutions.zip">the solution zip</a>.</p>
+      </section>
       <section>
-      <h1>Preliminaries 2019</h1>
+      <h1>Locations of preliminaries 2020</h1>
       <ul>
       <li><a href="https://commissies.ch.tudelft.nl/chipcie/?page=dapc/dapc2019">Delft University of Technology (DAPC)</a></li>
       <li>Eindhoven University of Technology (EAPC)</li>
