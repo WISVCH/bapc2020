8e9806178267e0cc6cf23ecfb5610ae8dea7d4c0
raoulesd <raoulesd@gmail.com>
2020-11-26 19:58:40 +0100

    Update bapc-footer.template.ts
    added asml logo to website footer

--- src/components/bapc-footer/bapc-footer.template.ts
+++ src/components/bapc-footer/bapc-footer.template.ts
@@ -7,6 +7,7 @@ export default function template(this: BapcFooter) {
     <h3>BAPC 2020 is hosted by:</h3>
     <div class="supporters">
     <a href="https://ch.tudelft.nl/"><img src="/images/ch.png" alt="Christiaan Huygens"/></a>
+    <a href="http://www.asml.com/students"><img class="tudelft" src="/images/asml.jpg" alt="ASML"/></a>
     <a href="https://tudelft.nl/en/"><img class="tudelft" src="/images/tudelft-logo.svg" alt="Delft University of Technology"/></a>
     </div>
 </footer>
