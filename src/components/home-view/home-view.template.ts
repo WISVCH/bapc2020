3be7300f2bd3a7ace0d3d6a47358b2553c774a3f
raoulesd <raoulesd@gmail.com>
2020-12-10 00:01:49 +0100

    Update home-view.template.ts
    gotta flex

--- src/components/home-view/home-view.template.ts
+++ src/components/home-view/home-view.template.ts
@@ -5,7 +5,7 @@ export default function template(this: BapcView) {
   return html`
     <section>
        <h1>About the BAPC</h1>
-            <p>The Benelux Algorithm Programming Contest (BAPC) is a contest in which about 50 teams from leading
+            <p>The Benelux Algorithm Programming Contest (BAPC) is a contest in which about 100 teams from leading
                 universities in Luxemburg, Belgium and the Netherlands are served a series of algorithmic
                 problems/puzzles. The goal of each team is to solve as many puzzles as possible within the set time
                 limit. Solutions need to be programmed out on a computer and can be submitted to a semi-automated
