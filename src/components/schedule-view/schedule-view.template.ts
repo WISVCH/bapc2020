f6d27c9ec7fe210276592c842fad9c4688934082
raoulesd <raoulesd@gmail.com>
2020-12-09 20:20:31 +0100

    Update schedule-view.template.ts
    update schedule to be coherent with information email sent out to contestants

--- src/components/schedule-view/schedule-view.template.ts
+++ src/components/schedule-view/schedule-view.template.ts
@@ -10,8 +10,8 @@ export default function template(this: BapcView) {
         <th>Time</th><th>Activity</th><th></th>
         <tr><td>&nbsp;8:30&mdash;9:30</td><td>'Doors' open</td><td></td></tr>
         <tr><td>9:30&mdash;10:00</td><td>Opening Ceremony</td><td></td></tr>
-        <tr><td>10:00&mdash;11:30</td><td>Test session <i>(For main <b>and</b> spectator contest)</i></td><td></td></tr>
-        <tr><td>11:30&mdash;12:00</td><td>Lunch, last minute remarks</td><td></td></tr>
+        <tr><td>10:00&mdash;11:00</td><td>Test session <i>(For main <b>and</b> spectator contest)</i></td><td></td></tr>
+        <tr><td>11:30&mdash;12:00</td><td>Last minute remarks</td><td></td></tr>
         <tr><td>12:00&mdash;17:00</td><td>Main Contest</td><td></td></tr>
         <tr><td>12:30&mdash;17:30</td><td>Spectator Contest</td><td></td></tr>
         <tr><td>17:30&mdash;18:00</td><td>Award Ceremony</td><td></td></tr>
