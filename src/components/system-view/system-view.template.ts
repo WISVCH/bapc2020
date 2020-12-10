a2db71aaea934313f223966fb9d7359d1b41a747
raoulesd <raoulesd@gmail.com>
2020-12-10 18:53:27 +0100

    Update system-view.template.ts

--- src/components/system-view/system-view.template.ts
+++ src/components/system-view/system-view.template.ts
@@ -11,7 +11,7 @@ export default function template(this: BapcView) {
         <li>Java: <code>openjdk version "11.0.9" 2020-10-20</code></li>
         <li>Python3: <code> [PyPy 7.0.0 with GCC 8.2.0]</code></li>
         <li>Kotlin: <code>Kotlin version 1.4.20-release-308 (JRE 11.0.9+11-post-Debian-1deb10u1).</code></li>
-        <li>C#(For company teams): <code>Mono JIT compiler version 5.18.0.240</code></li>
+        <li>C# (For company teams): <code>Mono JIT compiler version 5.18.0.240</code></li>
       </ul>
       <p>Note that Python 2 is no longer an official ICPC programming language and will therefore not be supported.</p>
       <p>For interactive problems we advice you have python locally installed on your machine.</p>
