acece0f4d61acada26eb41288cb06452ea73a02b
Verwoerd <thomas@keystone-strategic.com>
2020-12-12 17:39:09 +0100

    update scoreboard and problem links

--- src/components/problems-view/problems-view.template.ts
+++ src/components/problems-view/problems-view.template.ts
@@ -5,16 +5,16 @@ export default function template(this: BapcView) {
   return html`
     <section>
       <h2>BAPC 2020 Problems</h2>
-      <p>The following problems are available:</p>
+      <p>The following files are available:</p>
         <ul>
           <li><a href="http://commissies.ch.tudelft.nl/chipcie/archief/2020/bapc/problemset.pdf" target="_blank">Problem Set</a></li>
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Reference solutions and input/out</a></li>
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Solution Presentations</a></li>
+          <li><a href="http://commissies.ch.tudelft.nl/chipcie/archief/2020/bapc/solutions.zip" target="_blank">Reference solutions and input/out</a></li>
+          <li><a href="http://commissies.ch.tudelft.nl/chipcie/archief/2020/bapc/solutions.pdf" target="_blank">Solution Presentations</a></li>
         </ul>
     </section>
     <section>
       <h2>BAPC 2020 Preliminary Problems</h2>
-      <p>The following problems are available:</p>
+      <p>The following files are available:</p>
       <ul>
         <li><a href="http://commissies.ch.tudelft.nl/chipcie/archief/2020/dapc/problemset.pdf" target="_blank">Problems Set</a></li>
         <li><a href="http://commissies.ch.tudelft.nl/chipcie/archief/2020/dapc/solutions.zip" target="_blank">Reference solutions and input/out</a></li>
